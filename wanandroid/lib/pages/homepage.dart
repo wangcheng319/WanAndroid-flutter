7e2945a72e3fd41f285ba0ceb040beb9c546c4b1
wangc <274281610@qq.com>
2019-01-13 22:20:16 +0800

    11

--- wanandroid/lib/pages/homepage.dart
+++ wanandroid/lib/pages/homepage.dart
@@ -8,6 +8,7 @@ import 'dart:io';
 import '../data/banneritem.dart';
 import '../data/banner.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 ///主页面
 class HomePage extends StatefulWidget {
@@ -15,17 +16,12 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-
-  List<String> _banners;
+class _HomePageState extends State<HomePage> {
   BannerItem bannerItem;
   List datas = [];
 
   @override
   void initState() {
-    _controller = AnimationController(vsync: this);
     super.initState();
 
     getBanners();
@@ -33,92 +29,92 @@ class _HomePageState extends State<HomePage>
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQueryData.fromWindow(ui.window).size.width;
-    double screenHeight = MediaQueryData.fromWindow(ui.window).size.height;
-    print("+++屏幕宽度：$screenWidth");
-    _banners = [
-      "http://www.wanandroid.com/blogimgs/50c115c2-cf6c-4802-aa7b-a4334de444cd.png",
-      "http://www.wanandroid.com/blogimgs/fb0ea461-e00a-482b-814f-4faca5761427.png"
-    ];
     return Scaffold(
       appBar: AppBar(
         title: Text("首页"),
       ),
-      body: ListView.builder(
-        itemBuilder: _buildItem,
-        itemCount: datas.length,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: 200,
+            child: new Swiper(
+              itemCount: datas.length,
+              itemBuilder: _buildBanner,
+              pagination: new SwiperPagination(),
+              autoplay: true,
+              onTap: clickBanner,
+            ),
+          ),
+          Container(
+            child: new ListView.builder(
+              itemBuilder: _buildItem,
+              itemCount: datas.length,
+            ),
+            margin: const EdgeInsets.only(top: 200),
+          )
+        ],
       ),
     );
   }
 
+  /// 列表item
   Widget _buildItem(BuildContext context, int index) {
-    if (index == 0) {
-      return Container(
-        child: BannerView(data: _banners, buildShowView: _buildBanners),
-      );
-    } else {
-      return Card(
-        margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
-        child: Row(
-          children: <Widget>[
-            FadeInImage.memoryNetwork(
-              placeholder: kTransparentImage,
-              image: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3239375993,4053990160&fm=27&gp=0.jpg',
-              width: 50,
-              height: 50,
+    return Card(
+      margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+      child: Row(
+        children: <Widget>[
+          FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage,
+            image:
+                'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3239375993,4053990160&fm=27&gp=0.jpg',
+            width: 50,
+            height: 50,
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: <Widget>[
+                      Text(datas[index].title),
+                      Text("标题2"),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: <Widget>[
+                      Text("阅读量"),
+                      Text("时间"),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      children: <Widget>[
-                        Text(datas[index].title),
-                        Text("标题2"),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      children: <Widget>[
-                        Text("阅读量"),
-                        Text("时间"),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              flex: 1,
-            )
-          ],
-        ),
-      );
-    }
+            flex: 1,
+          )
+        ],
+      ),
+    );
   }
 
-  Widget _buildBanners(int index, itemData) {
+  /// banner
+  Widget _buildBanner(BuildContext context, int index) {
     return Image.network(
-      itemData,
+      datas[index].imagePath,
       fit: BoxFit.fill,
     );
   }
 
-  Widget _buildSwiper(BuildContext context, int index) {
-    return Image.network(
-      _banners[index],
-      fit: BoxFit.fill,
-    );
-  }
-
+  ///获取banner数据
   void getBanners() {
     HttpClient client = HttpClient();
     client
@@ -127,20 +123,20 @@ class _HomePageState extends State<HomePage>
       return request.close();
     }).then((HttpClientResponse response) {
       response.transform(utf8.decoder).listen((contents) {
-        // handle data
+        // 拿到json数据
         Map<String, dynamic> user = json.decode(contents);
+        //json解析
         bannerItem = BannerItem.fromJson(user);
-        print("==================");
-
-        print(bannerItem.data.first.url) ;
-        print(bannerItem.errorCode);
-
+        //刷新数据
         setState(() {
           datas = bannerItem.data;
         });
-
-        print(datas[1].title);
       });
     });
+  }
+
+  ///点击banner
+  void clickBanner(int index) {
+    print(datas[index].title);
   }
 }
--- wanandroid/lib/pages/projectpage.dart
+++ wanandroid/lib/pages/projectpage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:io';
 
 class ProjectPage extends StatefulWidget {
   @override
@@ -23,8 +25,53 @@ class _ProjectPageState extends State<ProjectPage>
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("项目"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("项目"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: 200,
+            child: new Swiper(
+              itemCount: 3,
+              itemBuilder: _buildBanner,
+              pagination: new SwiperPagination(),
+              autoplay: true,
+            ),
+          ),
+          Container(
+            child: new ListView.builder(
+              itemBuilder: _buildItem,
+              itemCount: 10,
+            ),
+            margin: const EdgeInsets.only(top: 200),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBanner(BuildContext context, int index) {
+    return Image.network(
+      "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3239375993,4053990160&fm=27&gp=0.jpg",
+      fit: BoxFit.fitWidth,
+    );
+  }
+
+  Widget _buildItem(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        color: Colors.yellow,
+        margin: const EdgeInsets.only(top: 30, left: 20, right: 20),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text("item$index"),
+        ),
+      ),
+      onTap: () {
+        print("item$index");
+      },
     );
   }
 }

ef5f8fe18514a8d65d200f0ac5b480e0e7057f4f
王成 <274281610@qq.com>
2019-01-19 17:44:10 +0800

    cookie

--- wanandroid/lib/pages/mepage.dart
+++ wanandroid/lib/pages/mepage.dart
@@ -6,7 +6,6 @@ import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:path_provider/path_provider.dart';
 
 import '../data/login_request.dart';
 import 'weixinpage.dart';
